c1e54081c53017f6df056120384c469ef4911eca
Daniel <danylo.ukl@gmail.com>
2017-04-06 21:47:16 +0300

    service es6 port

--- src/ng-compile-component-service.js
+++ src/ng-compile-component-service.js
@@ -1,63 +1,63 @@
-angular.module('rckd.utils').factory('CompileComponentService', [
-	'$rootScope',
-	'$compile',
-	function($rootScope, $compile){
-
-		/**
-		 * Transforms 'myFancyComponent' to 'my-fancy-component'.
-		 * 
-		 * @param  {String} string
-		 * @return {String}
-		 */
-		function toLowerDash(string){
-			return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
-		}
-
-		return{
+angular.module('rckd.utils')
+	.factory('CompileComponentService', [
+		'$rootScope',
+		'$compile',
+		function($rootScope, $compile) {
 
 			/**
-			 * Builds the component's html like:
+			 * Transforms 'myFancyComponent' to 'my-fancy-component'.
 			 *
-			 * <my-fancy-component
-			 * 	one-way='$ctrl["one-way"]'
-			 * 	two-way='$ctrl["two-way"]'
-			 * 	string='{{ $ctrl["string"] }}'
-			 * ></my-fancy-component>
-			 * 
-			 * @param  {String} component
-			 * @param  {Object} bindings
+			 * @param  {String} string
 			 * @return {String}
 			 */
-			render: function(component, bindings){
-				var tag = toLowerDash(component);
-				var attrs = '';
-				var prop = null;
-				for(prop in bindings){
-					attrs += ' ' + toLowerDash(prop) + '=\'' + (
-						typeof bindings[prop] === 'string'
-						? '{{ $ctrl["' + prop + '"] }}'
-						: '$ctrl["' + prop + '"]'
-					) + '\'';
+			const toLowerDash = (string) => string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+
+			return {
+
+				/**
+				 * Builds the component's html like:
+				 *
+				 * <my-fancy-component
+				 * 	one-way='$ctrl["one-way"]'
+				 * 	two-way='$ctrl["two-way"]'
+				 * 	string='{{ $ctrl["string"] }}'
+				 * ></my-fancy-component>
+				 *
+				 * @param  {String} component
+				 * @param  {Object} bindings
+				 * @return {String}
+				 */
+				render: (component, bindings) => {
+					const tag   = toLowerDash(component);
+					const attrs = bindings.reduce((attributes, currentAttr) => {
+
+						const attrString = `${toLowerDash(prop)}=\'(
+							${typeof bindings[currentAttr] === 'string'
+								? '{{ $ctrl["' + currentAttr + '"] }}' : '$ctrl["' + currentAttr + '"]'})\'`;
+						attributes.concat(attrString);
+
+						return attributes;
+					}, ' ');
+
+					return `<${tag}${attrs}></${tag}>`;
+				},
+
+				/**
+				 * Compiles the component.
+				 *
+				 * @param  {String} component
+				 * @param  {Object} bindings
+				 * @return {Object}
+				 */
+				compile: (component, bindings) => {
+					const html  = this.render(component, bindings);
+					const scope = angular.extend($rootScope.$new(), {
+						$ctrl: bindings
+					});
+					return $compile(html)(scope);
 				}
-				return '<' + tag + attrs + '></' + tag + '>';
-			},
 
-			/**
-			 * Compiles the component.
-			 * 
-			 * @param  {String} component
-			 * @param  {Object} bindings
-			 * @return {Object}
-			 */
-			compile: function(component, bindings){
-				var html = this.render(component, bindings);
-				var scope = angular.extend($rootScope.$new(), {
-					$ctrl: bindings
-				});
-				return $compile(html)(scope);
-			}
+			};
 
-		};
-
-	}
+		}
 ]);
